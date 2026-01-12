1233da0670931797412cc145802b995f6777d707
Samanja Cartagena <c.samanja09@Pabs.local>
2026-01-11 21:55:12 -0500

    search bar created

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import './App.css'
+import { SearchBar } from './components/SearchBar'
+
+function App() {
+
+  return (
+    <div className="App">
+      <div className='search-bar-container'>
+      <SearchBar />
+      <div>Search Results</div>
+      </div>
+    </div>
+  )
+}
+
+export default App
